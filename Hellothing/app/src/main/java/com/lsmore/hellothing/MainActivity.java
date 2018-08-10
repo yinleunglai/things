2cff710702203b4ab6178646a2aca4799d02af16
simon <liyanliang@lsmore.com>
2018-08-10 08:06:01 +0800

    gpio list

---
+++ Hellothing/app/src/main/java/com/lsmore/hellothing/MainActivity.java
@@ -0,0 +1,92 @@
+package com.lsmore.hellothing;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManager;
+
+import java.io.IOException;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ * <p>
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+public class MainActivity extends Activity {
+
+    private Gpio mButtonGpio;
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private static final String BUTTON_PIN_NAME = "BCM20"; // GPIO port wired to the button
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PeripheralManager peripheralManager = PeripheralManager.getInstance();
+        Log.d("home","list:" + peripheralManager.getGpioList());
+        Log.d("home","list:" + peripheralManager.getPwmList());
+        Log.d("home","list:" + peripheralManager.getI2cBusList());
+        Log.d("home","list:" + peripheralManager.getSpiBusList());
+        Log.d("home","list:" + peripheralManager.getUartDeviceList());
+
+
+        try {
+            // Step 1. Create GPIO connection.
+            mButtonGpio = peripheralManager.openGpio(BUTTON_PIN_NAME);
+            // Step 2. Configure as an input.
+            mButtonGpio.setDirection(Gpio.DIRECTION_IN);
+            // Step 3. Enable edge trigger events.
+            mButtonGpio.setEdgeTriggerType(Gpio.EDGE_FALLING);
+            // Step 4. Register an event callback.
+            mButtonGpio.registerGpioCallback(mCallback);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
+
+    }
+
+    // Step 4. Register an event callback.
+    private GpioCallback mCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            Log.i(TAG, "GPIO changed, button pressed");
+
+            // Step 5. Return true to keep callback active.
+            return true;
+        }
+    };
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Step 6. Close the resource
+        if (mButtonGpio != null) {
+            mButtonGpio.unregisterGpioCallback(mCallback);
+            try {
+                mButtonGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    }
+}
